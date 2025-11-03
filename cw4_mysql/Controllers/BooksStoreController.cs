13b3488737b091ae6fba5f59546e32013dd30c8a
Fuze0B <115667965+Fuze0B@users.noreply.github.com>
2025-11-03 15:05:40 +0100

    Add files via upload

---
+++ cw4_mysql/Controllers/BooksStoreController.cs
@@ -0,0 +1,67 @@
+using cw4_mysql.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace cw4_mysql.Controllers
+{
+    public class BooksStoreController : Controller
+    {
+        private BookRepo _db;
+        public BooksStoreController(IConfiguration config)
+        {
+            _db = new BookRepo(config);
+        }
+
+        // GET: BooksStoreController
+        public ActionResult Index()
+        {
+            var books = _db.GetBooks();
+            return View(books);
+        }
+        [HttpGet]
+        public IActionResult AddBook()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddBook(Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                //zapisanie do bazy
+                _db.AddBook(book);
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        public IActionResult Delete(int id)
+        {
+            //usuwanie z bazy
+            _db.DeleteBook(id);
+            return RedirectToAction("Index");    
+        }
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            // pobranie z bazy
+            var bookToEdit = _db.GetBookById(id);
+            if (bookToEdit == null)
+            {
+                // powrot do listy gdy nie znaleziono ksiazki do edycji
+                return RedirectToAction("Index");
+            }
+            //wysylanie do widoku modelu do edycji z  formularzem
+            return View(bookToEdit);
+        }
+        [HttpPost]
+        public IActionResult Edit(Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.UpdateBook(book);
+                return RedirectToAction("Index");
+            }
+            return View();
+        }   
+
+    }
+}
